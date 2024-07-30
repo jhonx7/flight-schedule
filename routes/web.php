e6bf5cbb45772ae89f11090a69926a0cf33a358d
Joni Setiawan <setiawanwhae@gmail.com>
2024-07-30 14:53:01 +0700

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
+use App\Http\Controllers\FlightController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+    return to_route('flight.index');
+})->name('home');
+
+Route::resource('flight', FlightController::class);
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -24,4 +22,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
